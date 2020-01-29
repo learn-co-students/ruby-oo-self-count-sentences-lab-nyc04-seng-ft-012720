1e3df7548497fcbbdfcc35b21940e5319046d598
mav-code <mavisansmembres@gmail.com>
2020-01-28 20:48:58 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
-require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    output = self.split("?")
+    output2 = output.map{|string| string.split("!")}.flatten
+    output3 = output2.map{|string| string.split(".")}.flatten
+    output3.length
   end
 end
